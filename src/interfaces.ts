a8914fa38ebd2a90cf07532f2d7b5ecb65b2f275
loulevin <patricia.balbier@gmail.com>
2024-02-22 11:33:38 +0100

    employees

---
+++ src/interfaces.ts
@@ -0,0 +1,6 @@
+export interface IEmployee {
+    employeeID: number;
+    firstName: string;
+    lastName: string;
+    title: string;
+}
--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react";
+import { IEmployee } from "../interfaces.ts";
+import * as config from "../config";
+import axios from "axios";
+
 export const PageWelcome = () => {
+	const [employees, setEmployees] = useState<IEmployee[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(`${config.backendUrl()}/employees`);
+			const _employees = response.data;
+			setEmployees(_employees);
+		})();
+	}, []);
+
 	return (
-		<p>This is the welcome page.</p>
-	)
-}
+		<>
+			<h2>There are {employees.length} employes:</h2>
+		</>
+	);
+};
